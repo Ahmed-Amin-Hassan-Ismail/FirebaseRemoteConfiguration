2f30d73628ef7100740f05bcedc2d3841442adbc
Ahmed Amin <AhmedAminHassanIsmail@gmail.com>
2023-07-04 18:29:52 +0300

    create new project

---
+++ FirebaseRemoteConfiguration/FirebaseRemoteConfiguration/FirebaseRemoteConfigurationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseRemoteConfigurationApp.swift
+//  FirebaseRemoteConfiguration
+//
+//  Created by Ahmed Amin on 04/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseRemoteConfigurationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
